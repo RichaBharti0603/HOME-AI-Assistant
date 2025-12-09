940128bd9b587b6184dc59d60f423a9c152fe3dd
RichaBharti0603 <richab820@gmail.com>
2025-12-09 19:06:29 +0530

    Cleaned repository and applied .gitignore

--- frontend/ui/app.py
+++ frontend/ui/app.py
@@ -1,8 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 import streamlit as st
 from backend.rag.rag import answer_query
-
 st.title("HOME — Local AI Assistant")
-
 query = st.text_input("Ask something:")
 if query:
     resp = answer_query(query)
